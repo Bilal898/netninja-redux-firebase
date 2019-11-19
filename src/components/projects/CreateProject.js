983ca9d545fee4f8a95270769aadcbf7d339f09c
bilal898 <bfhamid@gmail.com>
2019-11-19 08:20:14 +0500

    css added

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -3,10 +3,8 @@ import React, { Component } from 'react'
 export default class CreateProject extends Component {
 
     state = {
-        email: '',
-        password: '',
-        firstName: '',
-        LastName: ''
+        title: '',
+        content: '',
     }
 
     handleChange = (e) => {
@@ -23,21 +21,19 @@ export default class CreateProject extends Component {
     render() {
         return (
             <div className="container">
-                <form onSubmit={this.handleSubmit} className="white">
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-                    <div className="input-field">
-                        <label htmlFor="title">Title</label>
-                        <input type="text" id="title" onChange={this.handleChange}/>
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Content</label>
-<textarea>
-    </textarea>                    </div>
-    
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">
-                        Login</button>
-                    </div>
+                <form className="white" onSubmit={this.handleSubmit}>
+                <h5 className="grey-text text-darken-3">Create a New Project</h5>
+                <div className="input-field">
+                    <input type="text" id='title' onChange={this.handleChange} />
+                    <label htmlFor="title">Project Title</label>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="content">Project Content</label>
+                    <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+                </div>
+                <div className="input-field">
+                    <button className="btn pink lighten-1">Create</button>
+                </div>
                 </form>
             </div>
         )
